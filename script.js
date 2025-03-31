127a9e247aaed3fad5b16ca0f80a8a1e675db1bc
Mario Freire <mjfneto@gmail.com>
2025-03-31 15:54:21 -0300

    Feat: Implement audio transitions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const heroForm = document.getElementById('hero-form')
+const fieldSet = heroForm.querySelector('fieldset')
 const modal = document.getElementById('modal')
+const shieldBlock = document.getElementById('shield-block')
+const swordClash = document.getElementById('sword-clash')
+const swordImpact = document.getElementById('sword-impact')
+const swordSlash = document.getElementById('sword-slash')
 
 heroForm.addEventListener('submit', function (event) {
   event.preventDefault()
@@ -27,30 +32,55 @@ heroForm.addEventListener('submit', function (event) {
     lvl = 'Radiante'
   }
 
-  modal.insertAdjacentHTML(
-    'beforeend',
-    `
-        <div id="modal-content">
-            <div class="centered-content">
-                <p>O herói de nome 
-                <strong class="display-text">${hero}</strong>
-                está no nível
-                <strong class="display-text">${lvl}</strong>
-                </p>
-                
-            </div>
-        </div>
-    `
-  )
+  fieldSet.disabled = true
+
   modal.classList.add('visible')
-  modal.addEventListener('click', function (event) {
-    event.target.id == 'modal' && closeModal()
-  })
+
+  setTimeout(function () {
+    if (lvl === 'Ferro' || lvl === 'Bronze') {
+      shieldBlock.play()
+    } else if (lvl === 'Prata' || lvl === 'Ouro' || lvl === 'Platina') {
+      swordImpact.play()
+    } else if (
+      lvl === 'Ascendente' ||
+      lvl === 'Imortal' ||
+      lvl === 'Radiante'
+    ) {
+      swordClash.play()
+    }
+
+    modal.insertAdjacentHTML(
+      'beforeend',
+      `
+          <div id="modal-content">
+              <div class="centered-content">
+                  <p>O herói de nome 
+                  <strong class="display-text">${hero}</strong>
+                  está no nível
+                  <strong class="display-text">${lvl}</strong>
+                  </p>
+                  
+              </div>
+          </div>
+      `
+    )
+
+    modal.addEventListener('click', ifModalCloseModal)
+  }, 1000)
 })
 
 function closeModal() {
+  swordSlash.play()
+
   modal.classList.remove('visible')
 
   const modalContent = document.getElementById('modal-content')
   modalContent && modal.removeChild(modalContent)
+
+  modal.removeEventListener('click', ifModalCloseModal)
+}
+
+function ifModalCloseModal(event) {
+  fieldSet.disabled = false
+  event.target.id == 'modal' && closeModal()
 }
